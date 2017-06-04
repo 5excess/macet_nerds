76e26e1da67aafa71b51c20b22a7f108b3bf96cd
5excess <janosh3@gmail.com>
2017-06-04 18:59:52 +0300

    Начал вторую страницу.

--- js/script.js
+++ js/script.js
@@ -1,2 +1,14 @@
 var link = document.querySelector(".link-js");
-console.log ("profit");
+var modal = document.querySelector(".modal");
+var close = document.querySelector(".close");
+
+link.addEventListener("click", function(e) {
+    e.preventDefault();
+    modal.classList.add("show");
+})
+
+close.addEventListener("click", function() {
+    modal.classList.remove("show");
+})
+
+/*console.log ("profit");*/
